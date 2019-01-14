2bc67deb34de2bdaa25494197342d207e7e5c4b3
Shawnt Demirdjian <shawntdemirdjian@gmail.com>
2019-01-13 19:13:45 -0800

    Added Form layout. Added dynamic printing of saved people objects.

--- background.js
+++ background.js
@@ -1,3 +1,24 @@
+var people = [{
+	first: "John",
+	last: "Doe",
+	redID: "123456789",
+	email: "jdoe@sdsu.edu"
+}];
+
+chrome.runtime.onInstalled.addListener(function () {
+	// set people array to empty array if it's not found
+	// chrome.storage.sync.get(['people'], function (result) {
+	// 	if (typeof result.people == "undefined") {
+	// 		chrome.storage.sync.set({
+	// 			"people": []
+	// 		});
+	// 	}
+	// });
+	chrome.storage.sync.set({
+		"people": people
+	});
+})
+
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
 	if (message.request == "tabID") {
 		sendResponse({
--- popup.js
+++ popup.js
@@ -10,3 +10,26 @@ document.getElementById("start").addEventListener("click", () => {
 		});
 	});
 });
+
+$(document).ready(() => {
+	chrome.storage.sync.get(['people'], function (result) {
+		let checked = "";
+		result.people.forEach((element, index) => {
+			if (!index) {
+				checked = "checked"
+			}
+			$("#people").append(`
+				<div class='person'>
+					<input type="radio" id="${element.first}-${index}" value="${element.first}" name="people" ${checked}>
+					${element.first}
+					<br>
+					${element.last}
+					<br>
+					${element.email}
+					<br>
+					${element.redID}
+				</div>
+			`);
+		});
+	});
+});

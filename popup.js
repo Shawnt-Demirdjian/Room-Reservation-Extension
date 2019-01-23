67426151fb95aceb0f5ca8661d6cba4ac0c13dfd
Shawnt-Demirdjian <shawntdemirdjian@gmail.com>
2019-01-22 21:04:43 -0800

    Can now delete saved persons.

--- popup.js
+++ popup.js
@@ -1,45 +1,3 @@
-// On click Start Button
-$("#start").on("click", () => {
-	// TODO: validate form
-	// set form data
-	chrome.storage.local.set({
-		form: {
-			personIndex: $("input[name=people]:checked").val(),
-			startDate: $("#start-time").val(),
-			length: $("#length").val(),
-			room: $("#room").val()
-		}
-	}, function () {
-		// open new tab
-		chrome.tabs.create({
-			url: "https://sdsu.libcal.com/reserve/24-7-area",
-			active: false
-		}, (newTab) => {
-			chrome.storage.local.set({
-				"currTab": newTab.id
-			});
-		});
-	});
-});
-
-// On click Add Person button
-$("#add-person").on('click', function () {
-	let newPerson = {
-		'first': $("#add-first").val(),
-		'last': $("#add-last").val(),
-		'email': $("#add-email").val(),
-		'redID': $("#add-redID").val(),
-	};
-	chrome.storage.sync.get(['people'], function (result) {
-		result.people.push(newPerson);
-		chrome.storage.sync.set({
-			"people": result.people
-		}, function () {
-			window.location.href = "popup.html";
-		});
-	});
-});
-
 $(document).ready(() => {
 	chrome.storage.sync.get(['people'], function (result) {
 		let checked = "";
@@ -61,8 +19,67 @@ $(document).ready(() => {
 					${element.email}
 					<br>
 					${element.redID}
+					<button class="delete-person" value="${index}" >X</button>
 				</div>
 			`);
+
+			// On click Delete Person button
+			// This event listener is in this callback back the buttons to listen too are inserted just above
+			$(".delete-person").on('click', function () {
+				console.log("delete");
+				let personIndex = $(this).val();
+				chrome.storage.sync.get(['people'], function (result) {
+					result.people.splice(personIndex, 1);
+					chrome.storage.sync.set({
+						"people": result.people
+					}, function () {
+						window.location.href = "popup.html";
+					});
+				});
+			});
+		});
+	});
+
+	// On click Start Button
+	$("#start").on("click", () => {
+		// TODO: validate form
+		// set form data
+		chrome.storage.local.set({
+			form: {
+				personIndex: $("input[name=people]:checked").val(),
+				startDate: $("#start-time").val(),
+				length: $("#length").val(),
+				room: $("#room").val()
+			}
+		}, function () {
+			// open new tab
+			chrome.tabs.create({
+				url: "https://sdsu.libcal.com/reserve/24-7-area",
+				active: false
+			}, (newTab) => {
+				chrome.storage.local.set({
+					"currTab": newTab.id
+				});
+			});
+		});
+	});
+
+	// On click Add Person button
+	$("#add-person").on('click', function () {
+		// TODO: validate form
+		let newPerson = {
+			'first': $("#add-first").val(),
+			'last': $("#add-last").val(),
+			'email': $("#add-email").val(),
+			'redID': $("#add-redID").val(),
+		};
+		chrome.storage.sync.get(['people'], function (result) {
+			result.people.push(newPerson);
+			chrome.storage.sync.set({
+				"people": result.people
+			}, function () {
+				window.location.href = "popup.html";
+			});
 		});
 	});
 });
